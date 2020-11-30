b98fa18f14de4259a35083f4e91493d8fd92f50c
MFAC <ninafroes@gmail.com>
2020-11-30 19:54:42 +0100

    [refactor] Adjust code to eslint configuration

--- src/routers/event.ts
+++ src/routers/event.ts
@@ -1,20 +1,20 @@
-import express from "express"
+import express from 'express'
 
-import { isAuthenticated, isOwner } from "../middlewares/authentication"
+import { isAuthenticated, isOwner } from '../middlewares/authentication'
 import {
   createEvent,
   findEventById,
   findAllEvents,
   updateEvent,
-  deleteEvent,
-} from "../controllers/event"
+  deleteEvent
+} from '../controllers/event'
 
 const router = express.Router()
 
-router.post("/", isAuthenticated, createEvent)
-router.get("/", findAllEvents)
-router.get("/:eventId", findEventById)
-router.put("/:eventId", isOwner, updateEvent)
-router.delete("/:eventId", isOwner, deleteEvent)
+router.post('/', isAuthenticated, createEvent)
+router.get('/', findAllEvents)
+router.get('/:eventId', findEventById)
+router.put('/:eventId', isOwner, updateEvent)
+router.delete('/:eventId', isOwner, deleteEvent)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import app from "./app"
+import app from './app'
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-    console.log(
-        "  App is running at http://localhost:%d in %s mode",
-        app.get("port"),
-        app.get("env")
-    )
-    console.log("  Press CTRL-C to stop\n")
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    'App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  )
+  console.log('  Press CTRL-C to stop\n')
 })
 
 export default server
