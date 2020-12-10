56fc32b995884f61297cbf7849cf0a3dbcfaeb98
Mike Vedsted <44086320+MikeVedsted@users.noreply.github.com>
2020-12-10 10:29:17 +0200

    [FIX] Update seeds
    Updating seed to match changed DB structure

--- test/db/seeds.sql
+++ test/db/seeds.sql
@@ -12,216 +12,163 @@ DROP TABLE IF EXISTS comment;
 
 DROP TABLE IF EXISTS event;
 
-DROP TABLE IF EXISTS interest;
+DROP TABLE IF EXISTS category;
 
 DROP TABLE IF EXISTS userk;
 
 DROP TABLE IF EXISTS address;
 
-DROP TABLE IF EXISTS postal_code;
-
-DROP TABLE IF EXISTS city;
-
-DROP TABLE IF EXISTS country;
-
-CREATE TABLE country(
-  country_id SERIAL PRIMARY KEY,
-  country varchar(50) UNIQUE NOT NULL
-);
-
-CREATE TABLE city(
-  city_id SERIAL PRIMARY KEY,
-  city varchar(50) NOT NULL,
-  country int REFERENCES country(country_id) ON DELETE CASCADE
-);
-
-CREATE TABLE postal_code(
-  postal_code_id SERIAL PRIMARY KEY,
-  postal_code int NOT NULL,
-  city int REFERENCES city(city_id) ON DELETE CASCADE
-);
-
 CREATE TABLE address(
   address_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
   street varchar(50) NOT NULL,
   number int,
-  city int REFERENCES city(city_id) ON DELETE CASCADE,
-  postal_code int REFERENCES postal_code(postal_code_id) ON DELETE CASCADE
+  postal_code int NOT NULL,
+  city varchar(50) NOT NULL,
+  country varchar(50) NOT NULL,
+  lat numeric NOT NULL,
+  lng numeric NOT NULL
 );
 
-CREATE TABLE interest(
-  interest_id SERIAL UNIQUE PRIMARY KEY,
+CREATE TABLE category(
+  category_id SERIAL UNIQUE PRIMARY KEY,
   name varchar(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE userk(
   user_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-  first_name varchar(100) NOT NULL,
-  last_name varchar(50) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
   email varchar(50) UNIQUE NOT NULL,
-  profile_image varchar(255) DEFAULT 'https://res.cloudinary.com/dahevvjff/image/upload/v1606484281/l1dsr9hikb57ukge6ehk.jpg',
-  profile_text TEXT,
+  profile_image varchar(255),
+  profile_text varchar(255),
   base_address uuid REFERENCES address(address_id) ON DELETE CASCADE,
-  created_at TIMESTAMPTZ DEFAULT NOW()
+  created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE event(
   event_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
   title varchar(50) NOT NULL,
-  category int REFERENCES interest(interest_id) ON DELETE CASCADE,
+  category int REFERENCES category(category_id) ON DELETE CASCADE NOT NULL,
   date DATE NOT NULL,
   time TIME NOT NULL,
   description TEXT NOT NULL,
   max_participants int check(max_participants > 0) NOT NULL,
-  address uuid REFERENCES address(address_id) ON DELETE CASCADE,
-  created_by uuid REFERENCES userk(user_id) ON DELETE CASCADE,
-  expires_at TIMESTAMP NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  address uuid REFERENCES address(address_id) ON DELETE CASCADE NOT NULL,
+  created_by uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL,
+  expires_at DATE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   image varchar(255) DEFAULT 'https://res.cloudinary.com/dahevvjff/image/upload/v1606484900/wv6nzry10xk5h0vc0mgi.jpg'
 );
 
 CREATE TABLE event_request(
   er_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-  requester uuid REFERENCES userk(user_id) ON DELETE CASCADE,
-  event uuid REFERENCES event(event_id) ON DELETE CASCADE
+  requester uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL,
+  event uuid REFERENCES event(event_id) ON DELETE CASCADE NOT NULL,
+  requested_at TIMESTAMP DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE event_participant(
   ep_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-  participant uuid REFERENCES userk(user_id) ON DELETE CASCADE,
-  event uuid REFERENCES event(event_id) ON DELETE CASCADE
+  participant uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL,
+  event uuid REFERENCES event(event_id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE user_interest(
   ui_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE,
-  interest int REFERENCES interest(interest_id) ON DELETE CASCADE
+  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL,
+  interest int REFERENCES category(category_id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE admin(
   admin_id SERIAL PRIMARY KEY,
-  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE
+  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE banned_user(
   banned_user_id SERIAL PRIMARY KEY,
-  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE
+  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE comment(
   comment_id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
   comment text NOT NULL,
-  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE,
-  event uuid REFERENCES event(event_id) ON DELETE CASCADE
+  userk uuid REFERENCES userk(user_id) ON DELETE CASCADE NOT NULL,
+  event uuid REFERENCES event(event_id) ON DELETE CASCADE NOT NULL,
+  commented_at TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO
-  country (country)
+  address (
+    street,
+    number,
+    postal_code,
+    city,
+    country,
+    lat,
+    lng
+  )
 VALUES
-  ('Finland'),
-  ('Germany'),
-  ('Nepal'),
-  ('Brazil'),
-  ('Vietnam'),
-  ('Kazakhstan'),
-  ('Denmark');
+  (
+    'Street1',
+    1,
+    11111,
+    'Helsinki',
+    'Finland',
+    60.1234567,
+    50.1234567
+  ),
+  (
+    'Street2',
+    2,
+    22222,
+    'Tampere',
+    'Finland',
+    59.1234567,
+    50.1234567
+  ),
+  (
+    'Street3',
+    3,
+    33333,
+    'Oulu',
+    'Finland',
+    58.1234567,
+    50.1234567
+  ),
+  (
+    'Street4',
+    4,
+    44444,
+    'Berlin',
+    'Germany',
+    52.1234567,
+    50.1234567
+  ),
+  (
+    'Street5',
+    5,
+    55555,
+    'Hamburg',
+    'Germany',
+    51.1234567,
+    50.1234567
+  );
 
 INSERT INTO
-  city (city, country)
-VALUES
-  ('Helsinki', 1),
-('Oulu', 1),
-('Tampere', 1),
-('Berlin', 2),
-('Kathmandu', 3),
-('Lumbini', 3),
-('São Paulo', 4),
-('Belo Horizonte', 4),
-('Hanoi', 5),
-('Kon Tum', 5),
-('Karagandy', 6),
-('Aralsk', 6),
-('Nykøbing Falster', 7),
-('København', 7);
-
-INSERT INTO
-  postal_code (postal_code, city)
-VALUES
-  (12345, 1),
-  (98765, 1),
-  (33333, 1),
-  (12345, 2),
-  (98765, 2),
-  (33333, 2),
-  (12345, 3),
-  (98765, 3),
-  (33333, 3),
-  (12345, 4),
-  (98765, 4),
-  (33333, 4),
-  (12345, 5),
-  (98765, 5),
-  (33333, 5),
-  (12345, 6),
-  (98765, 6),
-  (33333, 6),
-  (12345, 7),
-  (98765, 7),
-  (33333, 7),
-  (12345, 8),
-  (98765, 8),
-  (33333, 8),
-  (12345, 9),
-  (98765, 9),
-  (33333, 9),
-  (12345, 10),
-  (98765, 10),
-  (33333, 10),
-  (12345, 11),
-  (98765, 11),
-  (33333, 11),
-  (12345, 12),
-  (98765, 12),
-  (33333, 12),
-  (12345, 13),
-  (98765, 13),
-  (33333, 13),
-  (12345, 14),
-  (98765, 14),
-  (33333, 14);
-
-INSERT INTO
-  address (street, number, city, postal_code)
-VALUES
-  ('Street1', 1, 1, 1),
-  ('Street2', 2, 2, 2),
-  ('Street3', 3, 3, 3),
-  ('Street4', 4, 4, 4),
-  ('Street5', 5, 5, 5),
-  ('Street6', 6, 6, 6),
-  ('Street7', 7, 7, 7),
-  ('Street8', 8, 8, 8),
-  ('Street9', 9, 9, 9),
-  ('Street10', 10, 10, 10),
-  ('Street11', 11, 11, 11),
-  ('Street12', 12, 12, 12),
-  ('Street13', 13, 13, 13),
-  ('Street14', 14, 14, 14);
-
-INSERT INTO
-  interest (name)
+  category (name)
 VALUES
   ('Football'),
   ('Tennis'),
-  ('Bouldering'),
   ('Basketball'),
-  ('Running');
+  ('Running'),
+  ('waterpolo');
 
 INSERT INTO
   userk (
     first_name,
     last_name,
     email,
+    profile_image,
     profile_text,
     base_address
   )
@@ -230,6 +177,7 @@ VALUES
     'Chiran',
     'Chapagain',
     'chiran@gmail.com',
+    'https://example.io/image.jpg',
     'Test user Chiran',
     (
       SELECT
@@ -244,6 +192,7 @@ VALUES
     'Marina',
     'Costa',
     'marina@gmail.com',
+    'https://example.io/image.jpg',
     'Test user Marina',
     (
       SELECT
@@ -258,6 +207,7 @@ VALUES
     'Nanna',
     'Dao',
     'nanna@gmail.com',
+    'https://example.io/image.jpg',
     'Test user Nanna',
     (
       SELECT
@@ -269,10 +219,11 @@ VALUES
     )
   ),
   (
-    'Rost',
-    'Petrenko',
-    'rost@gmail.com',
-    'Test user Rost',
+    'Mike',
+    'Vedsted',
+    'mike@gmail.com',
+    'https://example.io/image.jpg',
+    'Test user Mike',
     (
       SELECT
         address_id
@@ -283,10 +234,11 @@ VALUES
     )
   ),
   (
-    'Mike',
-    'Vedsted',
-    'mike@gmail.com',
-    'Test user Mike',
+    'Rost',
+    'Petrenko',
+    'rost@gmail.com',
+    'https://example.io/image.jpg',
+    'Test user Rost',
     (
       SELECT
         address_id
@@ -307,7 +259,8 @@ INSERT INTO
     max_participants,
     address,
     created_by,
-    expires_at
+    expires_at,
+    image
   )
 VALUES
   (
@@ -333,7 +286,8 @@ VALUES
       WHERE
         first_name = 'Chiran'
     ),
-    '10-10-2021'
+    '10-10-2021',
+    'https://example.io/image.jpg'
   ),
   (
     'Test title 2',
@@ -341,7 +295,7 @@ VALUES
     '01-07-2021',
     '04:00:00',
     'Test description 2',
-    1,
+    11,
     (
       SELECT
         address_id
@@ -356,9 +310,10 @@ VALUES
       FROM
         userk
       WHERE
-        first_name = 'Chiran'
+        first_name = 'Marina'
     ),
-    '10-10-2021'
+    '10-10-2021',
+    'https://example.io/image.jpg'
   ),
   (
     'Test title 3',
@@ -381,9 +336,10 @@ VALUES
       FROM
         userk
       WHERE
-        first_name = 'Marina'
+        first_name = 'Nanna'
     ),
-    '10-10-2021'
+    '10-10-2021',
+    'https://example.io/image.jpg'
   ),
   (
     'Test title 4',
@@ -391,7 +347,7 @@ VALUES
     '01-07-2021',
     '04:00:00',
     'Test description 4',
-    9,
+    8,
     (
       SELECT
         address_id
@@ -406,9 +362,10 @@ VALUES
       FROM
         userk
       WHERE
-        first_name = 'Marina'
+        first_name = 'Rost'
     ),
-    '10-10-2021'
+    '10-10-2021',
+    'https://example.io/image.jpg'
   ),
   (
     'Test title 5',
@@ -416,7 +373,7 @@ VALUES
     '01-07-2021',
     '04:00:00',
     'Test description 5',
-    99,
+    13,
     (
       SELECT
         address_id
@@ -431,134 +388,10 @@ VALUES
       FROM
         userk
       WHERE
-        first_name = 'Nanna'
-    ),
-    '10-10-2021'
-  ),
-  (
-    'Test title 6',
-    1,
-    '01-07-2021',
-    '04:00:00',
-    'Test description 6',
-    21,
-    (
-      SELECT
-        address_id
-      FROM
-        address
-      WHERE
-        number = 6
-    ),
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Nanna'
-    ),
-    '10-10-2021'
-  ),
-  (
-    'Test title 7',
-    2,
-    '01-07-2021',
-    '04:00:00',
-    'Test description 7',
-    1,
-    (
-      SELECT
-        address_id
-      FROM
-        address
-      WHERE
-        number = 7
-    ),
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Rost'
-    ),
-    '10-10-2021'
-  ),
-  (
-    'Test title 8',
-    3,
-    '01-07-2021',
-    '04:00:00',
-    'Test description 8',
-    5,
-    (
-      SELECT
-        address_id
-      FROM
-        address
-      WHERE
-        number = 8
-    ),
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Rost'
-    ),
-    '10-10-2021'
-  ),
-  (
-    'Test title 9',
-    4,
-    '01-07-2021',
-    '04:00:00',
-    'Test description 9',
-    9,
-    (
-      SELECT
-        address_id
-      FROM
-        address
-      WHERE
-        number = 9
-    ),
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
         first_name = 'Mike'
     ),
-    '10-10-2021'
-  ),
-  (
-    'Test title 10',
-    5,
-    '01-07-2021',
-    '04:00:00',
-    'Test description 10',
-    99,
-    (
-      SELECT
-        address_id
-      FROM
-        address
-      WHERE
-        number = 10
-    ),
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Mike'
-    ),
-    '10-10-2021'
+    '10-10-2021',
+    'https://example.io/image.jpg'
   );
 
 INSERT INTO
@@ -579,25 +412,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 3'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Chiran'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 4'
+        title = 'Test title 3'
     )
   ),
   (
@@ -615,25 +430,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 5'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Marina'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 6'
+        title = 'Test title 4'
     )
   ),
   (
@@ -651,25 +448,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 7'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Nanna'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 8'
+        title = 'Test title 5'
     )
   ),
   (
@@ -687,25 +466,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 9'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Rost'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 10'
+        title = 'Test title 1'
     )
   ),
   (
@@ -723,25 +484,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 1'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Mike'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 2'
+        title = 'Test title 2'
     )
   );
 
@@ -763,25 +506,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 6'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Chiran'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 7'
+        title = 'Test title 4'
     )
   ),
   (
@@ -799,25 +524,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 8'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Marina'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 9'
+        title = 'Test title 5'
     )
   ),
   (
@@ -835,25 +542,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 10'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Nanna'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 1'
+        title = 'Test title 1'
     )
   ),
   (
@@ -871,25 +560,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 2'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Rost'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 3'
+        title = 'Test title 2'
     )
   ),
   (
@@ -907,25 +578,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test titel 4'
-    )
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Mike'
-    ),
-    (
-      SELECT
-        event_id
-      FROM
-        event
-      WHERE
-        title = 'Test titel 5'
+        title = 'Test title 3'
     )
   );
 
@@ -986,61 +639,6 @@ VALUES
         first_name = 'Mike'
     ),
     5
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Chiran'
-    ),
-    3
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Marina'
-    ),
-    1
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Nanna'
-    ),
-    4
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Rost'
-    ),
-    5
-  ),
-  (
-    (
-      SELECT
-        user_id
-      FROM
-        userk
-      WHERE
-        first_name = 'Mike'
-    ),
-    2
   );
 
 INSERT INTO
@@ -1185,7 +783,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test title 6'
+        title = 'Test title 1'
     )
   ),
   (
@@ -1204,7 +802,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test title 7'
+        title = 'Test title 4'
     )
   ),
   (
@@ -1223,7 +821,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test title 8'
+        title = 'Test title 1'
     )
   ),
   (
@@ -1242,7 +840,7 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test title 9'
+        title = 'Test title 2'
     )
   ),
   (
@@ -1261,6 +859,6 @@ VALUES
       FROM
         event
       WHERE
-        title = 'Test title 10'
+        title = 'Test title 3'
     )
   );
