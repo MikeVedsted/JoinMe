4aab1dae867bd99cb72101ea05601dea221bf7a7
Rostislav013 <rostislav013@gmail.com>
2020-12-11 13:54:46 +0200

    [fix] Adjust dbhelper for jest

--- test/controllers/user.test.ts
+++ test/controllers/user.test.ts
@@ -20,12 +20,13 @@ describe('user controller', () => {
 
   it('should get all users', async () => {
     await db.query(
-      'INSERT INTO userk (user_id, first_name, last_name, email) VALUES ($1, $2, $3, $4);',
+      'INSERT INTO userk (user_id, first_name, last_name, email, created_at) VALUES ($1, $2, $3, $4, $5);',
       [
         '40e6215d-b5c6-4896-987c-f3063678f608',
         'Tester22',
         'Testerov',
-        'testherethestthere@test.com'
+        'testherethestthere@test.com',
+        '2017-03-18 08:21:36.175627+07'
       ]
     )
     const res = await request(app).get('/api/v1/users').send()
