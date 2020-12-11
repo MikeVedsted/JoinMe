2565ba002e7a828d5c1a9a530476b2f7624361eb
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-12-11 12:54:52 +0200

    [feat] Add landing page
    Responsive landing page created

---
+++ client/src/components/LandingPage/index.tsx
@@ -0,0 +1,41 @@
+import bodyParser from 'body-parser'
+import React from 'react'
+
+import Button from '../Button'
+import './LandingPage.scss'
+
+const LandingPage = () => {
+  return (
+    <div className="body">
+      <div className="body__circles">
+        <div className="body__circle1">
+          <p className="body__highlight">
+            Table Tennis Basketball, Horse Riding
+          </p>
+          <p className="body__text">
+            ..and 28 other events are available in Helsinki which you can join!
+          </p>
+        </div>
+        <div className="body__button">
+          <Button
+            type="button"
+            text="Get started!"
+            style="secondary"
+            handleClick={() => console.log('button clicked!')}
+          />
+        </div>
+        <div className="body__circle2">
+          <p className="body__highlight">
+            50 people already found event partners
+          </p>
+          <p className="body__text">
+            ..and hundreds more are waiting for you to create events and play
+            together!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
