83504db878884534adab9917fb100767d1cffae3
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-12-14 16:19:16 +0200

    [feat] Add homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 
-import GoogleUserLogin from './components/GoogleUserLogin'
-import GoogleAutoComplete from './components/GoogleAutoComplete'
-import EventForm from './components/EventForm'
+// import GoogleUserLogin from './components/GoogleUserLogin'
+// import GoogleAutoComplete from './components/GoogleAutoComplete'
+// import EventForm from './components/EventForm'
+import Homepage from './components/Homepage'
 import './App.scss'
 
 const App = () => {
   return (
-    <>
-      <h1>Moro Moro!</h1>
+    <div className="body">
+      <Homepage />
+      {/* <h1>Moro Moro!</h1>
       <p>Now start developing!</p>
       <GoogleUserLogin />
       <br />
       <h3>Create event form</h3>
-      <EventForm />
-    </>
+      <EventForm /> */}
+    </div>
   )
 }
 
---
+++ client/src/components/Homepage/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Homepage.scss'
+
+const Homepage = () => {
+  return (
+    <div className="main">
+      <div className="main__searchBox">
+        <h1>search events</h1>
+        <h1>search events</h1>
+        <h1>search events</h1>
+      </div>
+      <div className="main__events">
+        <h1>lists of events</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
