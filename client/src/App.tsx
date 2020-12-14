adb850ba4e3938805e4077c2371a44c31258f813
Mike Vedsted <mikevedsted@gmail.com>
2020-12-14 19:28:08 +0200

    [FEAT] Add modal example to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Modal from './components/Modal'
 import GoogleUserLogin from './components/GoogleUserLogin'
-import GoogleAutoComplete from './components/GoogleAutoComplete'
 import EventForm from './components/EventForm'
 import './App.scss'
 
 const App = () => {
+  const [closeModal, setCloseModal] = useState(false)
+
   return (
     <>
       <h1>Moro Moro!</h1>
       <p>Now start developing!</p>
-      <GoogleUserLogin />
+      {closeModal && (
+        <Modal
+          closeModal={() => setCloseModal(false)}
+          component={<GoogleUserLogin />}
+        />
+      )}
+      <button onClick={() => setCloseModal(true)}>
+        Click here to try out an amazing modal!
+      </button>
       <br />
       <h3>Create event form</h3>
       <EventForm />
