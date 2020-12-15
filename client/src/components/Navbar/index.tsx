6f398dc123d674577ada01598d509fa8002e0c64
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-12-15 11:00:11 +0200

    [feat] Add navigation bar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,52 +1,44 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell } from '@fortawesome/free-solid-svg-icons'
-import { faComment } from '@fortawesome/free-solid-svg-icons'
+import { faBell, faComment } from '@fortawesome/free-solid-svg-icons'
 
+import logo from '../../Assets/logo.svg'
 import './Navbar.scss'
-import logo from './logo2.png'
 
 const Navbar = () => {
-  const [nav, setNav] = useState(true)
+  const [navBg, setNavBg] = useState(true)
+  // FIX Later add const {userId} coming from res.ccokies
+  const userId = 1
   const [logged, setLogged] = useState(true)
 
-  const changeBG = () => {
-    if (window.scrollY >= 80) {
-      setNav(false)
-    } else {
-      setNav(true)
-    }
+  const changeNavStyle = () => {
+    window.scrollY >= 80 ? setNavBg(false) : setNavBg(true)
   }
 
-  window.addEventListener('scroll', changeBG)
+  window.addEventListener('scroll', changeNavStyle)
 
   return (
-    <header className={nav ? 'header' : 'header header--active'}>
-      <img
-        className="header__image header__image--logo"
-        src={logo}
-        alt="logo"
-      />
+    <nav className={navBg ? 'nav' : 'nav nav--active'}>
+      <img className="nav__image nav__image--logo" src={logo} alt="logo" />
       {logged ? (
-        <div className="header__icons">
-          <FontAwesomeIcon className="header__icon" icon={faBell} />
-          <FontAwesomeIcon className="header__icon" icon={faComment} />
-          <img
-            className="header__image header__image--profile"
-            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="profile"
-          />
+        <div className="nav__icons">
+          <FontAwesomeIcon className="nav__icon" icon={faBell} />
+          <FontAwesomeIcon className="nav__icon" icon={faComment} />
+          <Link to={`/${userId}`}>
+            <img
+              className="nav__image nav__image--profile"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              alt="profile"
+            />
+          </Link>
         </div>
       ) : (
-        <h2
-          className={
-            !logged ? 'header__login' : 'header__login header__login--hide'
-          }
-        >
+        <h2 className={!logged ? 'nav__login' : 'nav__login nav__login--hide'}>
           Login
         </h2>
       )}
-    </header>
+    </nav>
   )
 }
 
