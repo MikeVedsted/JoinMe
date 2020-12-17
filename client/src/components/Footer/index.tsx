3f0e4ba305c6cd28f08b1f9b094ccd731cac1ec4
Rostislav013 <rostislav013@gmail.com>
2020-12-17 12:17:06 +0200

    [refactor] change logo to dark + make footer sticky

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-import logo from '../../Assets/logoLight.svg'
+import logo from '../../Assets/logoDark.svg'
 import './Footer.scss'
 
 const Footer = () => {
