49fb644d8186df1c6fc592483c9a7caf2c17ad36
Mike Vedsted <mikevedsted@gmail.com>
2020-12-21 09:39:22 +0200

    Clean up front end routes

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,27 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 
 import ProtectedRoute from './util/ProtectedRoute'
 import LandingPage from './pages/LandingPage'
-import ProfilePage from './pages/ProfilePage'
+// import ProfilePage from './pages/ProfilePage'
 import MyEventsPage from './pages/MyEventsPage'
-
-const RegularComponent = () => {
-  return <h1>regular component 1</h1>
-}
-
-const ProtectedComponent = () => {
-  return <h1>protected component</h1>
-}
 
 const Routes = () => (
   <Switch>
     <Route exact path='/' component={LandingPage} />
-    <Route exact path='/test-regular' component={RegularComponent} />
-    <ProtectedRoute
-      exact
-      path='/test-protected'
-      component={ProtectedComponent}
-    />
-    <Route exact path='/user/:userId' component={ProfilePage} />
+    {/* <Route exact path='/user/:userId' component={ProfilePage} /> */}
     <Route path='/user/:userId/:contentName' component={MyEventsPage} />
   </Switch>
 )
