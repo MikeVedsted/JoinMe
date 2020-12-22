f6b6399f5cb722544e24fb78e488ef5123d68685
Mike Vedsted <mikevedsted@gmail.com>
2020-12-22 15:15:15 +0200

    Fix import typo for EventForm.scss

--- client/src/components/EventForm/index.tsx
+++ client/src/components/EventForm/index.tsx
@@ -8,7 +8,7 @@ import DropdownField from '../FormDropdownField'
 import GoogleAutoComplete from '../GoogleAutoComplete'
 import { useFormFields } from '../../hooks/useFormFields'
 import { eventCategories } from '../../util/constants/eventCategories'
-import './eventForm.scss'
+import './EventForm.scss'
 
 const EventForm = () => {
   const [cookies, setCookies] = useCookies(['user'])
