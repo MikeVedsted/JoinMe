edd35d219f0904680f260be4a27d3a500bd62798
Nanna Dao <daohoangngan1997@gmail.com>
2021-01-05 00:33:02 +0200

    [fix] resolve comments

--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useCookies } from 'react-cookie'
 import { useHistory, useParams } from 'react-router-dom'
 
 import Button from '../../components/Button'
-import Navbar from '../../components/Navbar'
 import { ProfilePageParamProps } from '../../types'
 import './ProfilePage.scss'
 
 const ProfilePage = () => {
   const history = useHistory()
+  const [cookies] = useCookies(['user'])
   const { userId } = useParams<ProfilePageParamProps>()
   const [userInfo, setUserInfo] = useState({
     profile_image: '',
@@ -43,53 +44,55 @@ const ProfilePage = () => {
   }
 
   return (
-    <>
-      <div className='profile'>
-        <div className='profile__sidebar'>
-          <img
-            src={userInfo.profile_image}
-            alt='Profile'
-            className='profile__sidebar--image'
-          />
-          <Button
-            type='button'
-            text='Edit profile'
-            modifier='primary'
-            onClick={handleEditClick}
-          />
+    <div className='profile'>
+      <div className='profile__sidebar'>
+        <img
+          src={userInfo.profile_image}
+          alt='Profile'
+          className='profile__sidebar--image'
+        />
+        <Button
+          type='button'
+          text='Edit profile'
+          modifier='primary'
+          onClick={handleEditClick}
+        />
+      </div>
+      <div className='profile__main'>
+        <div className='profile__header'>
+          <hr className='profile__header--divider-optional' />
+          <h3 className='profile__header--text'> Personal Details </h3>
+          <hr className='profile__header--divider' />
         </div>
-        <div className='profile__main'>
-          <div className='profile__header'>
-            <hr className='profile__header--divider-optional' />
-            <h3 className='profile__header--text'> Personal Details </h3>
-            <hr className='profile__header--divider' />
-          </div>
-          <div className='profile__details'>
-            <h4 className='profile__details--header'> Full Name: </h4>
-            <p className='profile__details--text'>
-              {' '}
-              {`${userInfo.first_name} ${userInfo.last_name}`}{' '}
-            </p>
-          </div>
+        <div className='profile__details'>
+          <h4 className='profile__details--header'> Full Name: </h4>
+          <p className='profile__details--text'>
+            {' '}
+            {`${userInfo.first_name} ${userInfo.last_name}`}{' '}
+          </p>
+        </div>
 
+        {cookies.user.user_id === userId && (
           <div className='profile__details'>
             <h4 className='profile__details--header'> Email: </h4>
             <p className='profile__details--text'> {userInfo.email} </p>
           </div>
+        )}
 
-          <div className='profile__details'>
-            <h4 className='profile__details--header'> Interests: </h4>
-            <p className='profile__details--text'>
-              {' '}
-              {userInfo.interests?.join(', ')}{' '}
-            </p>
-          </div>
+        <div className='profile__details'>
+          <h4 className='profile__details--header'> Interests: </h4>
+          <p className='profile__details--text'>
+            {' '}
+            {userInfo.interests?.join(', ')}{' '}
+          </p>
+        </div>
 
-          <div className='profile__details'>
-            <h4 className='profile__details--header'> Intro: </h4>
-            <p className='profile__details--text'> {userInfo.profile_text} </p>
-          </div>
+        <div className='profile__details'>
+          <h4 className='profile__details--header'> Intro: </h4>
+          <p className='profile__details--text'> {userInfo.profile_text} </p>
+        </div>
 
+        {cookies.user.user_id === userId && (
           <div className='profile__details'>
             <h4 className='profile__details--header'> Address: </h4>
             {userInfo.base_address ? (
@@ -101,9 +104,9 @@ const ProfilePage = () => {
               <p className='profile__details--text'> - </p>
             )}
           </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
