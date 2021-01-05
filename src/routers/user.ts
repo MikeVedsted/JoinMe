dc66d441872e4f9e7ca1a5f7b97e1f3a257dc81a
Rostislav013 <rostislav013@gmail.com>
2021-01-05 20:41:55 +0200

    [feat] Protect user routes

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,20 +1,14 @@
 import express from 'express'
 
-import { isAuthenticated } from '../middlewares/authentication'
-import {
-  findUserById,
-  findAllUsers,
-  updateUser,
-  googleLogin,
-  deleteUser
-} from '../controllers/user'
+import { isAuthenticated, isOwner } from '../middlewares/authentication'
+import { findUserById, updateUser, googleLogin, deleteUser } from '../controllers/user'
 
 const router = express.Router()
 
-router.get('/', findAllUsers)
-router.get('/:userId', findUserById)
+router.get('/:userId', isAuthenticated, isOwner, findUserById)
+// FIX create issue: Add one more route for limited user info (not owner)
 router.post('/google-authenticate', googleLogin)
-router.put('/:userId', isAuthenticated, updateUser)
-router.delete('/:userId', isAuthenticated, deleteUser)
+router.put('/:userId', isAuthenticated, isOwner, updateUser)
+router.delete('/:userId', isAuthenticated, isOwner, deleteUser)
 
 export default router
