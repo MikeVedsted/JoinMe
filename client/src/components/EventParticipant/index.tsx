fcdbd0ca6bfb2d5a5a953e8e87caa8bf51a99c1b
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-05 17:29:09 +0200

    [Feat] Add participant component
    Fixes; destructure, styles

--- client/src/components/EventParticipant/index.tsx
+++ client/src/components/EventParticipant/index.tsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { EventParticipantProps } from '../../types'
+import { EventParticipantProps, UserType } from '../../types'
 import './EventParticipant.scss'
 
 const EventParticipant = ({ user, handleDelete }: EventParticipantProps) => {
+  const { profile_image, user_id, first_name, last_name } = user && user
   return (
     <div className='participant'>
       <img
         className='participant__image'
-        src={user.profile_image}
-        alt='profile'
+        src={profile_image}
+        alt={first_name}
       />
-      <Link className='participant__link' to={`/${user.user_id}`}>
-        <p className='participant__name'>{`${user.first_name} ${user.last_name}`}</p>
+      <Link className='participant__link' to={`/${user_id}`}>
+        <p className='participant__name'>{`${first_name} ${last_name}`}</p>
       </Link>
       <FontAwesomeIcon
         onClick={handleDelete}
