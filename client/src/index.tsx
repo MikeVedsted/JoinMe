f22e04a02500f76e70b510b773b7385e23a3ae81
Rostislav013 <rostislav013@gmail.com>
2021-01-05 20:17:32 +0200

    [refactor] move icon lindrary to its own file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 
 import App from './App'
+import './util/constants/fontawesome'
 import makeStore from './redux/store'
 import './index.scss'
 
