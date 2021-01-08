928682af51cbe562c9185916a697103eb4a85cf9
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-08 16:04:35 +0200

    [Feat] Add participants and requests modal

--- client/src/pages/MyEventsPage/index.tsx
+++ client/src/pages/MyEventsPage/index.tsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 import MyEventsSidebar from '../../components/MyEventsSidebar'
-import Modal from '../../components/Modal'
-import EventParticipantsAndRequests from '../../components/EventParticipantsAndRequests'
 import EventForm from '../../components/EventForm'
-import useUserDisplay from '../../hooks/useUserDisplay'
 import './MyEventsPage.scss'
 
 const MyEventsPage = () => {
   const { pathname } = useLocation()
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [users] = useUserDisplay()
 
   const addContent = () => {
-    if (pathname.includes('/created'))
-      return <p onClick={() => setIsModalOpen(true)}>My events here</p>
+    if (pathname.includes('/created')) return <p>My events here</p>
     if (pathname.includes('/interested')) return <p>Requested events here</p>
     if (pathname.includes('/confirmed')) return <p>Confirmed events here</p>
     if (pathname.includes('/create-new')) return <EventForm />
@@ -27,22 +21,7 @@ const MyEventsPage = () => {
       <div className='my-events__controls'>
         <MyEventsSidebar />
       </div>
-      <div className='my-events__content'>
-        <div>
-          {addContent()}
-          {isModalOpen && (
-            <Modal
-              closeModal={() => setIsModalOpen(false)}
-              content={
-                <EventParticipantsAndRequests
-                  participants={users}
-                  joinRequests={users}
-                />
-              }
-            />
-          )}
-        </div>
-      </div>
+      <div className='my-events__content'>{addContent()}</div>
     </div>
   )
 }
