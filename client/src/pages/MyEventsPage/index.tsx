87ee812f9d61fec9df70382cbd633b32771bf497
Mike Vedsted <mikevedsted@gmail.com>
2021-01-08 13:13:52 +0200

    Add in eventslist for interested events

--- client/src/pages/MyEventsPage/index.tsx
+++ client/src/pages/MyEventsPage/index.tsx
@@ -2,15 +2,24 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 
 import MyEventsSidebar from '../../components/MyEventsSidebar'
+import EventList from '../../components/EventList'
 import EventForm from '../../components/EventForm'
+import useInterestedEvents from '../../hooks/useInterestedEvents'
 import './MyEventsPage.scss'
 
 const MyEventsPage = () => {
   const { pathname } = useLocation()
+  const [interestedEvents] = useInterestedEvents()
 
   const addContent = () => {
     if (pathname.includes('/created')) return <p>My events here</p>
-    if (pathname.includes('/interested')) return <p>Requested events here</p>
+    if (pathname.includes('/interested'))
+      return (
+        <EventList
+          title={'Events you have requested to join'}
+          events={interestedEvents}
+        />
+      )
     if (pathname.includes('/confirmed')) return <p>Confirmed events here</p>
     if (pathname.includes('/create-new')) return <EventForm />
     return null
