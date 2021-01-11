5627c0533e04d2ca3923b99eadecfccfd8d01a47
Mike Vedsted <mikevedsted@gmail.com>
2021-01-11 09:41:12 +0200

    [fix] Resolve review comments
    Add spaces to scss

--- client/src/util/helperFunctions.tsx
+++ client/src/util/helperFunctions.tsx
@@ -2,16 +2,16 @@ export const calculateEventAge = (eventDate: string) => {
   const diff = new Date().getTime() - new Date(eventDate).getTime()
   const hours = parseInt((diff / 3600000).toFixed(0))
   if (hours <= 24) {
-    return hours + ' hours'
+    return hours + ' hour(s)'
   } else if (hours > 24 && hours <= 730) {
     const days = (hours / 24).toFixed(0)
-    return days + ' days'
+    return days + ' day(s)'
   } else if (hours > 730 && hours <= 8640) {
     const months = (hours / 730).toFixed()
-    return months + ' months'
+    return months + ' month(s)'
   } else {
     const years = (hours / 8640).toFixed(0)
-    return years + ' years'
+    return years + ' year(s)'
   }
 }
 
