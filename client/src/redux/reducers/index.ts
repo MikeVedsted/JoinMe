27941e658a0cf7ca204b10f0a27ea500deab4256
Nanna Dao <daohoangngan1997@gmail.com>
2021-01-20 21:59:48 +0200

    [feat] redux

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 
 import user from './user'
+import event from './event'
 // import auth from './auth'
 // import error from './error'
 
 const createRootReducer = () =>
   combineReducers({
-    user
+    user,
+    event
     // auth,
     // error,
   })
