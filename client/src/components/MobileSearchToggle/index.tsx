a92ab6e983a9f07d16c85caa0d216c0f239f6da2
Mike Vedsted <mikevedsted@gmail.com>
2021-01-25 18:45:10 +0200

    [chore] Alignment fixes

--- client/src/components/MobileSearchToggle/index.tsx
+++ client/src/components/MobileSearchToggle/index.tsx
@@ -4,18 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { toggleModal } from '../../redux/actions'
 import { screenGreaterThan } from '../../util/helperFunctions'
-import { AppState } from '../../Types'
+import { MobileSearchToggleProps, AppState } from '../../Types'
 import './MobileSearchToggle.scss'
 
-const MobileSearchToggle = () => {
+const MobileSearchToggle = ({ toggle, state }: MobileSearchToggleProps) => {
   const dispatch = useDispatch()
   const { hideModal } = useSelector((state: AppState) => state.ui)
+
+  const handleClick = () => {
+    toggle(!state)
+    dispatch(toggleModal(hideModal))
+  }
 
   return (
     <div
       hidden={screenGreaterThan(768)}
       className='mobile-search-toggle'
-      onClick={() => dispatch(toggleModal(hideModal))}
+      onClick={handleClick}
     >
       <FontAwesomeIcon icon='filter' />
     </div>
--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import Modal from '../../components/Modal'
@@ -9,24 +9,30 @@ import MobileSearchToggle from '../../components/MobileSearchToggle'
 import { screenGreaterThan } from '../../util/helperFunctions'
 import { AppState } from '../../Types'
 import './Homepage.scss'
+import Loading from '../../components/Loading'
 
 const Homepage = () => {
   const { allEvents } = useSelector((state: AppState) => state.event)
   const { hideModal } = useSelector((state: AppState) => state.ui)
+  const { loading } = useSelector((state: AppState) => state.loading)
+  const [mobileSearch, setMobileSearch] = useState(false)
 
   return (
     <div className='homepage'>
-      <MobileSearchToggle />
+      <MobileSearchToggle toggle={setMobileSearch} state={mobileSearch} />
 
-      {/* Search box modal for small screens */}
-      {!hideModal && <Modal content={<EventSearch distance='30' />} />}
+      {!hideModal && mobileSearch && (
+        <Modal content={<EventSearch distance='30' />} />
+      )}
 
       <div className='homepage__left-column' hidden={!screenGreaterThan(768)}>
         <EventSearch distance='100' />
       </div>
 
       <div className='homepage__right-column'>
-        {allEvents && allEvents.length > 0 ? (
+        {loading ? (
+          <Loading />
+        ) : allEvents && allEvents.length > 0 ? (
           <EventList events={allEvents} />
         ) : (
           <NotFound message='Sorry, no events found.' />
