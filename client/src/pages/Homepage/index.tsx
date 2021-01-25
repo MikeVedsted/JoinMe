b2740c5cd85348ab7cad41071957148a90d26bce
Mike Vedsted <mikevedsted@gmail.com>
2021-01-25 11:30:33 +0200

    [refactor] Remove component logics, inject redux

--- client/src/pages/Homepage/index.tsx
+++ client/src/pages/Homepage/index.tsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Modal from '../../components/Modal'
 import NotFound from '../../components/NotFound'
 import EventList from '../../components/EventList'
 import EventSearch from '../../components/EventSearch'
 import MobileSearchToggle from '../../components/MobileSearchToggle'
-import useEventDisplay from '../../hooks/useEventDisplay'
 import { screenGreaterThan } from '../../util/helperFunctions'
 import { AppState } from '../../Types'
 import './Homepage.scss'
@@ -15,19 +13,18 @@ import './Homepage.scss'
 const Homepage = () => {
   const { allEvents } = useSelector((state: AppState) => state.event)
   const { hideModal } = useSelector((state: AppState) => state.ui)
-  
+
   return (
     <div className='homepage'>
       <MobileSearchToggle />
 
       {/* Search box modal for small screens */}
-      {!hideModal && (
-        <Modal content={<EventSearch distance='30' />} />
-      )}
+      {!hideModal && <Modal content={<EventSearch distance='30' />} />}
 
       <div className='homepage__left-column' hidden={!screenGreaterThan(768)}>
-        <EventSearch distance='100'/>
+        <EventSearch distance='100' />
       </div>
+
       <div className='homepage__right-column'>
         {allEvents && allEvents.length > 0 ? (
           <EventList events={allEvents} />
