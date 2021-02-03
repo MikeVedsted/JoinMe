6402dbf75e975c665aeb1f502bf48cce2d53f518
Mike Vedsted <mikevedsted@gmail.com>
2021-02-03 09:54:04 +0200

    Add open path matching for production

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,10 @@ app.use('/api/v1/comments', commentRouter)
 app.use('/api/v1/requests', requestRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
 }
 
 export default app
